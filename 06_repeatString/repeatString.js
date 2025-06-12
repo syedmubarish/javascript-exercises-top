a855c301e954d7ea887f0859fc748bebd58aad33
Sayed Mubarish <mubarish007@gmail.com>
2025-06-12 17:32:09 +0530

    solve 06-repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+    let newStr = ""
+    if(n<0) newStr = "ERROR"
+    else if(n==0) newStr = ""
+    else{
+        while(newStr.length != str.length*n){
+            newStr+=str
+        }
+    }
+    
+    return newStr
 };
 
 // Do not edit below this line
