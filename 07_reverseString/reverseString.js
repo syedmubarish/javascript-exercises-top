0b5149d7d838b0825bdd90969156025646880a18
Sayed Mubarish <mubarish007@gmail.com>
2025-06-12 17:58:32 +0530

    solve 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split("");
+  arr.reverse()
+  let newStr = arr.join("");
+  return newStr;
 };
 
 // Do not edit below this line
