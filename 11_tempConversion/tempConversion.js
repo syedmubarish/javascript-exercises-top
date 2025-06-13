ebacda4ad0e2b41735e8ac82bb488f530551d61f
Sayed Mubarish <mubarish007@gmail.com>
2025-06-13 15:08:30 +0530

    solve_11_tempCoversions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return +(5/9 * (temp - 32)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return +(9/5*temp + 32).toFixed(1)
 };
 
 // Do not edit below this line
