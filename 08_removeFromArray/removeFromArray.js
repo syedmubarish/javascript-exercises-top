de562321081b1e9ca17969b2934eaa129e55e7da
Sayed Mubarish <mubarish007@gmail.com>
2025-06-13 12:35:51 +0530

    solve 08-remove-from-array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    // const newArr = []
+    // arr.forEach(item => {
+    //     if(!args.includes(item)){
+    //         newArr.push(item)
+    //     }
+    // })
+    // return newArr
+
+    return arr.filter(item =>{
+        return( !args.includes(item))
+    })
+
 };
 
 // Do not edit below this line
