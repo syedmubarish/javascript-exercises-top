a5d21e5a6da2230f848031c637738aa482e3292e
Sayed Mubarish <mubarish007@gmail.com>
2025-06-18 10:57:26 +0530

    solve-fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    if(term<0) return "OOPS"
+    else if(term==0)return 0
+    else if(term==1||term==2)return 1
+    else{
+        return fibonacci(term - 1) + fibonacci(term - 2)
+    }
 };
 
 // Do not edit below this line
