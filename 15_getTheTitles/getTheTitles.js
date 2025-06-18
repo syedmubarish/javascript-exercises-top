843e5e9103ec3c35d351bbe4d20a55d2a3ee9da6
Sayed Mubarish <mubarish007@gmail.com>
2025-06-18 11:04:27 +0530

    solve-15-getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((acc,curr)=>{
+        acc.push(curr.title)
+        return acc
+    },[])
 };
 
 // Do not edit below this line
