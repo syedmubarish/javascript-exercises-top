f0f34201a7b1a51b49b9d24f86fc2108afa12bfb
Sayed Mubarish <mubarish007@gmail.com>
2025-06-18 10:14:11 +0530

    solve-12-calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((acc,curr)=>{
+    acc+=curr
+    return acc
+  },0)
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((a,b)=>{
+    return a-b
+  })
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,curr)=>{
+    acc+=curr
+    return acc
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b)=>{
+    return a*b
+  })
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a==0){
+
+    return 1
+  }else{
+
+    return a * factorial(a - 1) 
+  }
 };
 
 // Do not edit below this line
