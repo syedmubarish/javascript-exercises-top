c638b34805550e4f4fccb32c68e1e6702f10a128
Sayed Mubarish <mubarish007@gmail.com>
2025-06-18 10:49:11 +0530

    solve-13-palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphabetsNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
+  const filteredStringArr = string
+    .split("")
+    .filter((value) => alphabetsNumbers.includes(value));
+
+    const filteredString = filteredStringArr.join("").toUpperCase()
+    const reversedString = filteredStringArr.reverse().join("").toUpperCase()
+
+    if(filteredString==reversedString)return true
+    return false
 };
 
 // Do not edit below this line
