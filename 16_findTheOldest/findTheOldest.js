a1f863dcb3a764b0cbc71040f2e7f9d6d8e6a418
Sayed Mubarish <mubarish007@gmail.com>
2025-06-18 11:27:57 +0530

    solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((a,b)=>{
+        if(!a.yearOfDeath){
+            a.yearOfDeath = new Date().getFullYear()
+        }
+        
+        if(!b.yearOfDeath){
+            b.yearOfDeath = new Date().getFullYear()
 
+        }
+
+        const ageOfA=a.yearOfDeath-a.yearOfBirth
+        const ageOfB=b.yearOfDeath-b.yearOfBirth
+        if(ageOfA>ageOfB)return a
+        else return b
+    },{})
 };
 
 // Do not edit below this line
